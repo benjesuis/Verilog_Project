eb88d4f2f0f8e31a9a504b59b2904bfa8ad1191c
benjesuis <32913438+benjesuis@users.noreply.github.com>
2019-04-21 17:22:35 -0400

    Add files via upload

---
+++ VGADispaly.v
@@ -0,0 +1,45 @@
+module vga_display(
+  input rst,
+  input clk,
+
+  output [2:0]R,
+  output [2:0]G,
+  output [1:0]B,
+  output HS,
+  output VS,
+  output reg light
+);
+  
+   
+  wire [10:0] hcount, vcount; // hcount is line position, vcount is screen position
+  wire blank;
+  wire clk_25Mhz;
+  
+  clock_divider clk_div_25 (
+   .clock_in(clk),
+   .reset(rst),
+   .clock_out(clk_25Mhz)
+  );
+
+  vga_controller_640_60 vc(
+    .rst(rst),
+	 .pixel_clk(clk_25Mhz),
+	 .HS(HS),
+	 .VS(VS),
+	 .hcounter(hcount),
+	 .vcounter(vcount),
+	 .blank(blank)
+  );
+
+
+  always @(posedge clk_25Mhz) begin
+    light <= ~blank;
+  end
+		  
+  assign R[2] = {3{(hcount >0) & (hcount < 640) & (vcount > 0) & (vcount < 480) ? 1:0}};
+  assign R[1:0] = {0};
+  assign G[2:0] = {0};
+  assign B[1:0] = {0};
+
+ 
+endmodule
